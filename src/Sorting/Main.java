68bb59c2382634344a476b7289b62e88a463c265
stevo <kondinskis@gmail.com>
2014-12-14 02:10:56 +0100

    update

--- src/Sorting/Main.java
+++ src/Sorting/Main.java
@@ -10,23 +10,27 @@ public class Main {
 	public static void main(String[] args) {
 		final int x = 10;
 		int[] array = new int[x+1];
-		
-		FillArray fill = new FillArray(x, array);
-		fill.PrintArray(x, array);
-		
-		System.out.println();
-		
-		SortArray sort = new SortArray(x, array);
-		sort.PrintSortedArray(x, array);
-		System.out.println();
-		
-		
-		System.out.println();
-		
+			
 		try(BufferedWriter bw = new BufferedWriter(new FileWriter(new File("SortedArrays.txt"), true))){
-			for(int i=1;i<=x;i++)
-			bw.write(array[i] + " ");
+			bw.write("Normal: ");
 			bw.newLine();
+			FillArray fill = new FillArray(x, array);
+			fill.PrintArray(x, array);
+			System.out.println();
+			for(int i=0;i<x;i++)
+				bw.write(array[i] + " ");
+			bw.newLine();
+			
+			SortArray sort = new SortArray(x, array);
+			sort.PrintSortedArray(x, array);
+			System.out.println();
+			
+			bw.write("Sorted: ");
+			bw.newLine();
+			for(int i=1;i<=x;i++)
+				bw.write(array[i] + " ");
+			bw.newLine();
+			
 		}
 		catch(Exception e){
 			System.out.println(e.getMessage());
